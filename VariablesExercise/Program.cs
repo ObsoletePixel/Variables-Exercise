c111cf32d76e7b7614ce67727c634c9d9b56ac4f
ObsoletePixel <obsoletepixel@gmail.com>
2025-03-21 15:13:57 -0700

    First commit for the Variables-Exercise project

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Sadie";
+            int dogAge = 4;
+            char dogGender = 'f';
+            bool isDog = true;
+            
+            //dotnet 9.0 has an issue with this being declared as a decimal, for some reason.
+            //I've converted this to a double so it works, but if it's necessary to make this a decimal let me know and I'll troubleshoot"
+            //Appending the "M" suffix to the variable still didn't let me manually set its data type as a decimal."
+            double dogWeight = 5.3;
+            
+            double dogFoodPrice = 39.99;
+            Console.WriteLine($"My dog's name is {dogName}. She is {dogAge} years old. Her gender is {dogGender}, and she is {dogWeight} kg. Her food costs {dogFoodPrice}");
         }
     }
 }
